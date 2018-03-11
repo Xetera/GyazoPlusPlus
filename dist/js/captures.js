f5196f4f36db222ed3d5d9920f45d6b9dc133b07
Ali <mythologyali@gmail.com>
2018-03-11 11:03:38 -0700

    fully working

--- dist/js/captures.js
+++ dist/js/captures.js
@@ -60,42 +60,179 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 3:
+/******/ ([
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-chrome.runtime.onMessage.addListener(function (msg) {
-    console.log('received fetch request');
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "capture")) {
-        /* Call the specified callback, passing
-           the web-pages DOM content as argument */
-        console.log('fetching pictures');
-        let objects = document.querySelectorAll(`.thumb`);
-        console.log(objects);
-        editCaptures(objects);
-    }
-});
+function checkDuplicate(alias) {
+    return new Promise(function (resolve, reject) {
+        chrome.storage.local.get(alias, function (items) {
+            console.log('checking dupe');
+            console.log(Object.keys(items).length ? 'dupe' : 'not dupe');
+            Object.keys(items).length ? reject('duplicate picture') : resolve();
+        });
+    });
+}
+exports.checkDuplicate = checkDuplicate;
+function getAllPictures() {
+    return new Promise(function (resolve) {
+        chrome.storage.local.get(null, function (items) {
+            console.log('Got all items from storage!');
+            console.log(items);
+            resolve(items);
+        });
+    });
+}
+exports.getAllPictures = getAllPictures;
+function getPicture(alias) {
+    chrome.storage.local.get(null, function (items) {
+        console.log(items);
+        return items;
+    });
+}
+exports.getPicture = getPicture;
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = __webpack_require__(0);
 function* getElements(collection) {
     for (let i = 0; i < collection.length; ++i) {
         yield collection[i];
     }
 }
-function editCaptures(cards) {
-    cards.forEach(function (value, index, listObj) {
-        console.log(value);
+function getFromCollectionByClass(elem, className) {
+    const elements = Array.from(elem);
+    for (let i in elements) {
+        console.log('current element');
+        console.log(elements[i]);
+        if (elements[i].classList.contains(className)) {
+            return elements[i];
+        }
+    }
+}
+function getCardMetadata(card) {
+    const lowerCard = card.parentNode.parentNode.childNodes[1];
+    if (lowerCard instanceof HTMLElement) {
+        return lowerCard;
+    }
+}
+function getParentCard(thumb) {
+    return thumb.parentElement.parentElement;
+}
+function getNoEntrySign(parent) {
+    console.log('getting entry signs');
+    return parent.children[3];
+}
+function getHref(parent) {
+    console.log('href');
+    console.log(parent.children[2].children[0]);
+    return parent.children[2];
+}
+function getHoverLayer(parent) {
+    const arr = Array.from(parent.children);
+    for (let elem of arr) {
+        if (elem.classList.contains('hover-layer'))
+            return elem;
+    }
+}
+function getDisabledOverlay(parent) {
+    const arr = Array.from(parent.children);
+    for (let elem of arr) {
+        if (elem.classList.contains('disabled-overlay')) {
+            return elem;
+        }
+    }
+}
+function removeElementClass(element, className) {
+    const classes = element.classList;
+    if (classes.contains(className)) {
+        classes.remove(className);
+    }
+}
+function editCaptures(thumbs) {
+    console.log(thumbs.length);
+    let storage;
+    database_1.getAllPictures().then(response => {
+        storage = response;
+        console.log('response gotten');
+        console.log(response);
+        return response;
+    }).then((response) => {
+        const thumbnails = Array.from(thumbs);
+        console.log(thumbnails);
+        const savedIds = Object.keys(response);
+        console.log(savedIds);
+        thumbnails.forEach(function (thumb) {
+            if (thumb instanceof HTMLElement) {
+                console.log(thumb.classList);
+                const lowerCard = getCardMetadata(thumb);
+                const parent = getParentCard(thumb);
+                const sign = getNoEntrySign(parent);
+                const hover = getHoverLayer(parent);
+                const disabled = getDisabledOverlay(parent);
+                console.log('metadata');
+                console.log(lowerCard);
+                //console.log(href);
+                if (sign)
+                    sign.remove();
+                if (hover)
+                    hover.remove();
+                if (disabled) {
+                    thumb.style.backgroundColor = '#a9a2a7';
+                    lowerCard.style.backgroundColor = '#a9a2a7';
+                }
+                else {
+                    thumb.style.backgroundColor = '#db2e42';
+                    lowerCard.style.backgroundColor = '#db2e42';
+                }
+                removeElementClass(parent, 'disabled');
+                const thumbId = thumb.getAttribute('src');
+                thumb.style.zIndex = '1000';
+                if (savedIds.indexOf(thumbId) > -1) {
+                    thumb.style.backgroundColor = '#00ffad';
+                    lowerCard.style.backgroundColor = '#00ffad';
+                    const layer = document.createElement(`div`);
+                    layer.setAttribute('title', response[thumbId]);
+                    layer.classList.add('separation-layer');
+                    layer.style.position = 'relative';
+                    layer.style.pointerEvents = 'none';
+                    layer.style.width = 'inherit';
+                    layer.style.height = 'inherit';
+                    layer.style.zIndex = '999';
+                    parent.insertBefore(layer, parent.children.item(1));
+                    //alert('hello');
+                }
+                //href.setAttribute('href', 'pricing')
+            }
+        });
+        return Array.from(document.getElementsByClassName('card'));
+    }).then(layers => {
+        layers.forEach(function (layer, index, array) {
+            layer.addEventListener('click', function (event) {
+                const children = Array.from(this.children);
+                for (let child of children) {
+                    if (child.classList.contains('separation-layer')) {
+                        location.href = child.getAttribute('title');
+                    }
+                }
+            });
+        });
     });
 }
 exports.editCaptures = editCaptures;
 
 
 /***/ })
-
-/******/ });
+/******/ ]);
--- src/captures.ts
+++ src/captures.ts
@@ -1,16 +1,8 @@
+import {Gyazo} from "./index";
+import GyazoStorage = Gyazo.GyazoStorage;
+import {getAllPictures} from "./database";
+import EntryMetadata = chrome.fileSystemProvider.EntryMetadata;
 
-chrome.runtime.onMessage.addListener(function(msg) {
-    console.log('received fetch request');
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "capture")) {
-        /* Call the specified callback, passing
-           the web-pages DOM content as argument */
-        console.log('fetching pictures');
-        let objects = document.querySelectorAll(`.thumb`);
-        console.log(objects);
-        editCaptures(objects);
-    }
-});
 
 function* getElements(collection : HTMLCollectionOf<Element>){
     for (let i = 0 ; i < collection.length; ++i){
@@ -18,8 +10,137 @@ function* getElements(collection : HTMLCollectionOf<Element>){
     }
 }
 
-export function editCaptures(cards : NodeListOf<Element>){
-    cards.forEach(function (value, index, listObj) {
-        console.log(value);
-    });
+function getFromCollectionByClass(elem : HTMLCollection , className : string) : Element {
+    const elements = Array.from(elem);
+    for (let i in elements){
+        console.log('current element');
+        console.log(elements[i]);
+        if (elements[i].classList.contains(className)){
+            return elements[i];
+        }
+    }
 }
+
+function getCardMetadata(card : Element){
+    const lowerCard = card.parentNode.parentNode.childNodes[1];
+    if (lowerCard instanceof HTMLElement){
+        return lowerCard;
+    }
+}
+
+function getParentCard(thumb : Element){
+    return thumb.parentElement.parentElement;
+}
+
+function getNoEntrySign(parent : Element) : Element {
+    console.log('getting entry signs');
+    return parent.children[3];
+}
+
+function getHref(parent : Element){
+    console.log('href');
+    console.log(parent.children[2].children[0]);
+    return parent.children[2];
+}
+
+function getHoverLayer(parent : Element){
+    const arr = Array.from(parent.children);
+    for (let elem of arr ){
+        if (elem.classList.contains('hover-layer'))
+            return elem;
+    }
+}
+
+function getDisabledOverlay(parent  : Element){
+    const arr = Array.from(parent.children);
+    for (let elem of arr){
+        if (elem.classList.contains('disabled-overlay')){
+            return elem;
+        }
+    }
+}
+
+function removeElementClass(element: Element, className : string){
+    const classes = element.classList;
+    if (classes.contains(className)){
+        classes.remove(className)
+    }
+}
+
+export function editCaptures(thumbs : NodeListOf<Element>){
+    console.log(thumbs.length);
+    let storage : GyazoStorage;
+
+    getAllPictures().then(response => {
+        storage = response;
+        console.log('response gotten');
+        console.log(response);
+        return response;
+    }).then((response : GyazoStorage )=>{
+        const thumbnails = Array.from(thumbs);
+        console.log(thumbnails);
+
+        const savedIds : string[] = Object.keys(response);
+        console.log(savedIds);
+        thumbnails.forEach(function (thumb) {
+            if (thumb instanceof HTMLElement){
+                console.log(thumb.classList);
+                const lowerCard = getCardMetadata(thumb);
+                const parent = getParentCard(thumb);
+                const sign = getNoEntrySign(parent);
+                const hover = getHoverLayer(parent);
+                const disabled = getDisabledOverlay(parent);
+                console.log('metadata');
+                console.log(lowerCard);
+                //console.log(href);
+                if (sign)
+                    sign.remove();
+                if (hover)
+                    hover.remove();
+                if (disabled){
+                    thumb.style.backgroundColor = '#a9a2a7';
+                    lowerCard.style.backgroundColor = '#a9a2a7';
+                }
+                else {
+                    thumb.style.backgroundColor = '#db2e42';
+                    lowerCard.style.backgroundColor = '#db2e42';
+                }
+                removeElementClass(parent,'disabled');
+                const thumbId = thumb.getAttribute('src');
+                thumb.style.zIndex = '1000';
+                if (savedIds.indexOf(thumbId) > -1){
+                    thumb.style.backgroundColor = '#00ffad';
+                    lowerCard.style.backgroundColor = '#00ffad';
+
+                    const layer = document.createElement(`div`);
+                    layer.setAttribute('title', response[thumbId]);
+                    layer.classList.add('separation-layer');
+
+                    layer.style.position = 'relative';
+                    layer.style.pointerEvents = 'none';
+
+                    layer.style.width = 'inherit';
+                    layer.style.height = 'inherit';
+
+                    layer.style.zIndex = '999';
+                    parent.insertBefore(layer, parent.children.item(1));
+                    //alert('hello');
+                }
+                //href.setAttribute('href', 'pricing')
+            }
+        });
+        return Array.from(document.getElementsByClassName('card'));
+    }).then(layers => {
+        layers.forEach(function(layer, index, array){
+            layer.addEventListener('click', function(event){
+                const children : Element[]= Array.from(this.children);
+                for (let child of children){
+                    if (child.classList.contains('separation-layer')){
+                        location.href = child.getAttribute('title');
+                    }
+                }
+            });
+        })
+    })
+}
+
